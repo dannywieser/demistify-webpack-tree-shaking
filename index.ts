7ef8e300f6a5b292bd01f8c12cd71191ca71e3b0
dannywieser <danny.wieser@blackboard.com>
2018-10-15 07:43:59 -0600

    fix tsconfig module/targets

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import get from 'lodash-es/get';
-import { includeOnlyUsedFunctionFromLodash } from 'moduleA/exportB'
+import { get } from 'lodash-es';
+import { includeOnlyUsedFunctionFromLodash } from 'moduleA';
 
 const anObject = { foo: 'bar' };
 console.log(get(anObject, 'foo')); // bar
